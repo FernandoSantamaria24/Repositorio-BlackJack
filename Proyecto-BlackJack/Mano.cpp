0ae7945787fb840e4acb831563188d8eb474265c
Angel QJ <angel.quesada.jimenez@est.una.ac.cr>
2024-01-24 20:00:57 -0600

    mano iniciador de cartas en nullptr y correcion de error en destrcutor

--- Proyecto-BlackJack/Mano.cpp
+++ Proyecto-BlackJack/Mano.cpp
@@ -2,12 +2,14 @@
 #include "Mazo.h"
 //Método constructor sin parámetros 
 Mano::Mano(){
-
+	for (int i = 0; i < 9; i++){
+		cartas[i] = nullptr;
+	}
+	cant = 0;
 }
 //Borra todas las cartas que se encuentran dentro de la mano de cada jugador
 //Impidiendo que ocurra un memory leak por parte de Mano
-Mano::~Mano()
-{
+Mano::~Mano(){
 	delete[] cartas;
 }
 //Agrega una carta a la mano del jugador seleccionado
@@ -23,19 +25,17 @@ void Mano::agregarCarta(Mazo* m){
 }
 //Limpia la mano para que el jugador pueda volver a jugar otra partida
 void Mano::limpiar(){
-	for (int i = 0; i < 9; i++)
-	{
+	for (int i = 0; i < 9; i++){
 		if (cartas[i]!=nullptr) {
 			cartas[i] = nullptr;
-	}
+		}
 	}
 }
 //Suma la cantidad de puntos que el usuario tiene hasta el momento
 int Mano::getPuntos()
 {
 	int sum = 0;
-	for (int i = 0; i < 9; i++)
-	{
+	for (int i = 0; i < 9; i++){
 		if (cartas[i]!=nullptr) {
 			sum += cartas[i]->getValor();
 		}
