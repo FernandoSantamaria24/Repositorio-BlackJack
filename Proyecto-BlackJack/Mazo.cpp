c2f54c6ad2a99b2edd3a9bf7e7cc9afbe28b871c
Angel QJ <angel.quesada.jimenez@est.una.ac.cr>
2024-02-11 10:26:28 -0600

    Arrregglos juego

--- Proyecto-BlackJack/Mazo.cpp
+++ Proyecto-BlackJack/Mazo.cpp
@@ -23,10 +23,10 @@ void Mazo::inicializador(){
 
     for (int i = 1; i <= 13; i++) {
         for (int j = 1; j <= 4; j++) {
-            carta[pos]->setValor(i);
-            carta[pos]->setPalo(j);
-            carta[pos]->voltear();
-            pos++;
+                carta[pos]->setValor(i);
+                carta[pos]->setPalo(j);
+                carta[pos]->voltear();
+                pos++;
         }
     }
 }
